5a9deea1fb087e3a6d57722fc713466f534e65f5
Theron Patrick <theron.patrick@ingramcontent.com>
2016-09-16 21:59:54 -0400

    project setup

---
+++ gyrados/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Oh hai
+      </div>
+    );
+  }
+}
+
+export default App;
